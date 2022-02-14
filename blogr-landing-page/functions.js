016b208e12d259963d821dcb90da98d8d9c9a765
simoneclr <simone.clclr@gmail.com>
2022-02-14 15:47:39 +0100

    Blogr-Landing-Page: Add mobile navigation menu

--- blogr-landing-page/functions.js
+++ blogr-landing-page/functions.js
@@ -19,9 +19,21 @@ window.onload = () => {
 
 	//Add Click event listener to body to close dropdowns when clicking elsewhere
 	const body = document.querySelector("body")
+
 	body.addEventListener("click", e => {
 		dropdowns.forEach(dropdown => {
 			dropdown.classList.remove("active")
 		})
 	}, {capture: false})
+
+	// Get mobile menu toggle button
+	const menuToggle = document.querySelector(".menu-toggle")
+
+	// Get the nav content to be toggled
+	const navContentToggle = document.querySelector("#nav-content-toggle")
+
+	menuToggle.addEventListener("click", e => {
+		menuToggle.classList.toggle("icon-active")
+		navContentToggle.classList.toggle("nav-content-active")
+	})
 }
